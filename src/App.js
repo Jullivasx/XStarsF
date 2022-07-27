db4af35fa26895e1cc50d9670f50a403d5c8ceb4
Jullivasx <jullivasx@gmail.com>
2022-07-27 06:56:47 +0300

    App update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import CaptionText from './components/CaptionText/CaptionText';
 import CardStar from './components/CardStar/CardStar';
 import ViewStar from './views/ViewStar/ViewStar';
 
 function App() {
   return (
     <div className="App">
-      <CardStar/>
       <ViewStar/>
     </div>
   );
